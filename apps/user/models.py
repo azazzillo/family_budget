98b6ed7a02cd20ecc2fceef7065639e6ed91e169
azazzillo <mariyash_zhumabekova@mail.ru>
2025-06-22 22:21:59 +0500

    create_superuser

--- apps/user/models.py
+++ apps/user/models.py
@@ -1,5 +1,5 @@
 # Django
-from django.contrib.auth.models import AbstractBaseUser, AbstractUser
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.contrib.auth.base_user import BaseUserManager
 from django.utils import timezone
 from django.db import models
@@ -12,8 +12,29 @@ class CustomUserManager(BaseUserManager):
     def get_by_natural_key(self, email):
         return self.get(email=email)
 
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('Email must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
 
-class CustomUser(AbstractBaseUser):
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_owner', True)
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if not extra_fields.get('is_staff'):
+            raise ValueError('Superuser must have is_staff=True.')
+        if not extra_fields.get('is_superuser'):
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self.create_user(email, password, **extra_fields)
+
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
 
     class Position(models.TextChoices):
         PARENT = "PARENT", "Parent"
@@ -45,9 +66,13 @@ class CustomUser(AbstractBaseUser):
         verbose_name="является ли владельцем",
         default=False
     )
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
 
     objects = CustomUserManager()
+
     USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
 
     class Meta:
         verbose_name = 'пользователь'
@@ -55,7 +80,13 @@ class CustomUser(AbstractBaseUser):
 
     def __str__(self):
         return f"{self.name} ({self.position})"
-    
+
+    def has_perm(self, perm, obj=None):
+        return self.is_superuser
+
+    def has_module_perms(self, app_label):
+        return self.is_superuser
+
 
 class Family(models.Model):
 
